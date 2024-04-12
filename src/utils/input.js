5614f1fc16f09a6c3601322c4c1fef0d04594498
buggluck <emiliendjafeur@gmail.com>
2024-04-12 14:42:50 +0200

    affichage du détails des clients

--- src/utils/input.js
+++ src/utils/input.js
@@ -1,5 +1,3 @@
-import { updateProductList } from '../utils/Get_Product.js';
-
 document.addEventListener('DOMContentLoaded', async function () {
     const searchInput = document.getElementById('searchInput');
     const productList = document.getElementById('product-list');
