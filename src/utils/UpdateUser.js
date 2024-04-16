d6a0a51e6cea7b0d0c064948cfdf4f57e56c8c31
buggluck <emiliendjafeur@gmail.com>
2024-04-16 11:02:03 +0200

    vous pouvez Update un user une seule fois

--- src/utils/UpdateUser.js
+++ src/utils/UpdateUser.js
@@ -1,29 +1,29 @@
-// import {ip} from '../conf.js';
+import {ip} from '../../config.js';
 
-// export async function updateUser(user_id, last_name, first_name, username, email, tel) {
-//     try {
-//         const response = await fetch(ip + '/user/update', {
-//             method: 'PUT',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Authorization': "Bearer " + sessionStorage.getItem('token')
-//             },
-//             body: JSON.stringify({user_id, last_name, first_name, username, email, tel})
-//         });
+export async function updateUser(user_id, last_name, first_name, username, email, tel) {
+    try {
+        const response = await fetch(ip + '/user/update', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + sessionStorage.getItem('token')
+            },
+            body: JSON.stringify({user_id, last_name, first_name, username, email, tel})
+        });
 
-//         const statusCode = response.status; // Récupérer le code HTTP
+        const statusCode = response.status; // Récupérer le code HTTP
 
-//         if (statusCode == 200) {
-//             return "User supprimé";
-//         } else {
-//             if (statusCode == 401) {
-//                 return "User inexistant";
-//             } else {
-//                 return "Error";
-//             }
-//         }
-//     } catch (error) {
-//         console.error('Erreur lors de la suppression de l\'utilisateur:', error);
-//         throw error; // Propager l'erreur pour la gérer à un niveau supérieur si nécessaire
-//     }
-// }
+        if (statusCode == 200) {
+            return "User supprimé";
+        } else {
+            if (statusCode == 401) {
+                return "User inexistant";
+            } else {
+                return "Error";
+            }
+        }
+    } catch (error) {
+        console.error('Erreur lors de la suppression de l\'utilisateur:', error);
+        throw error; // Propager l'erreur pour la gérer à un niveau supérieur si nécessaire
+    }
+}
