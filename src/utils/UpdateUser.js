d7d6c46109e90d345b388bf06dfae97946fe2567
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-04-16 19:23:45 +0200

    Updating the project and adding new files

--- src/utils/UpdateUser.js
+++ src/utils/UpdateUser.js
@@ -1,57 +1,45 @@
-import { ip } from '../../config.js';
+import {ip} from '../../config.js';
 
-export async function updateUser(user_id, last_name, first_name, username, email, tel) {
+export async function deleteUser(id_user) {
     try {
-        const response = await fetch(ip + '/user/update', {
-            method: 'PUT',
+        const response = await fetch(ip + '/user/delete', {
+            method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': "Bearer " + sessionStorage.getItem('token')
             },
-            body: JSON.stringify({ user_id, last_name, first_name, username, email, tel })
+            body: JSON.stringify({id_user})
         });
 
         const statusCode = response.status; // Récupérer le code HTTP
 
         if (statusCode == 200) {
-            return "User mis à jour";
+            return "User supprimé";
         } else {
             if (statusCode == 401) {
                 return "User inexistant";
             } else {
-                // Récupérer le corps de la réponse pour obtenir plus d'informations sur l'erreur
-                const responseBody = await response.json();
-                return `Erreur lors de la mise à jour de l'utilisateur : ${responseBody.message}`;
+                return "Error";
             }
         }
     } catch (error) {
-        console.error('Erreur lors de la mise à jour de l\'utilisateur:', error);
+        console.error('Erreur lors de la suppression de l\'utilisateur:', error);
         throw error; // Propager l'erreur pour la gérer à un niveau supérieur si nécessaire
     }
 }
+// Récupérer les paramètres de l'URL
+const queryString1 = window.location.search;
+const urlParams1 = new URLSearchParams(queryString1);
 
-document.addEventListener('DOMContentLoaded', async function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const clientId = urlParams.get('id');
+// Récupérer la valeur de l'ID
+const id1 = urlParams1.get('id');
 
-    try {
-        // Votre code pour récupérer les données du client
+const deleteButton = document.querySelector('.btn-danger');
 
-        // Ajouter l'écouteur d'événements au formulaire
-        document.getElementById('updateUserForm').addEventListener('submit', async function (event) {
-            event.preventDefault(); // Empêche le rechargement de la page
-
-            const last_name = document.getElementById('client-last-name').textContent;
-            const first_name = document.getElementById('client-first-name').textContent;
-            const username = document.getElementById('client-username').textContent;
-            const email = document.getElementById('client-email').textContent;
-            const tel = document.getElementById('client-tel').textContent;
-
-            const response = await updateUser(clientId, last_name, first_name, username, email, tel);
-
-            alert("Changement effectué");
-        });
-    } catch (error) {
-        console.error('Error:', error);
-    }
+// Ajout d'un gestionnaire d'événements au clic sur le bouton
+deleteButton.addEventListener('click', function() {
+  // Code à exécuter lorsque le bouton est cliqué
+  if (confirm("Voulez-vous vraiment supprimer cet utilisateur ?")) {
+    const response = deleteUser(id1);
+  }
 });
