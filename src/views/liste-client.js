d9c3a31ccd3bf063386c06030c26c40509e4fb34
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-05-15 21:29:07 +0200

    update liste client

--- src/views/liste-client.js
+++ src/views/liste-client.js
@@ -1,4 +1,20 @@
 export default () => `
     <h1>Liste Client</h1>
-
+    <div class="content">
+        <h1>Liste des Clients</h1>
+        <input type="search" id="searchInput" placeholder="rechercher un client">
+        <table id="client-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nom</th>
+              <th>Prénom</th>
+              <th>Téléphone</th>
+              <th>Email</th>
+              <th>Nom d'utilisateur</th>
+            </tr>
+          </thead>
+          <tbody id="client-list"></tbody>
+        </table>
+      </div>
 `;
