c49c78a5b227802df79e43d677d769f208c18755
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-05-16 08:56:22 +0200

    update css files of the login page

--- src/views/liste-client.js
+++ src/views/liste-client.js
@@ -1,20 +1,22 @@
-export default () => `
-    <h1>Liste Client</h1>
+export default function ListeClient() {
+  return `
+  <link rel="stylesheet" href="./common.css" />
     <div class="content">
-        <h1>Liste des Clients</h1>
-        <input type="search" id="searchInput" placeholder="rechercher un client">
-        <table id="client-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nom</th>
-              <th>Prénom</th>
-              <th>Téléphone</th>
-              <th>Email</th>
-              <th>Nom d'utilisateur</th>
-            </tr>
-          </thead>
-          <tbody id="client-list"></tbody>
-        </table>
-      </div>
-`;
+      <h1>Liste des Clients</h1>
+      <input type="search" id="searchInput" placeholder="rechercher un client" />
+      <table id="client-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Téléphone</th>
+            <th>Email</th>
+            <th>Nom d'utilisateur</th>
+          </tr>
+        </thead>
+        <tbody id="client-list"></tbody>
+      </table>
+    </div>
+  `;
+}
